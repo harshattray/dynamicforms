aeff13f4885a24ebc0d6166539d79711549107f4
Harsha Attray <harsh.attray@gmail.com>
2018-09-17 16:31:40 +0530

    Shallow test cases added

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,13 +2,13 @@
  * @Author: harsha
  * @Date:   2018-09-17T13:13:17+05:30
  * @Last modified by:   harsha
- * @Last modified time: 2018-09-17T14:42:30+05:30
+ * @Last modified time: 2018-09-17T16:30:40+05:30
  */
 import React from "react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
 import toJson from "enzyme-to-json";
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { shallow, mount } from "enzyme";
 import App from "../../App";
 import SubmitDynamicForms from "../DynamicForms/DynamicForms";
 import Adapter from "enzyme-adapter-react-16";
@@ -24,4 +24,22 @@ it("App component mounts", () => {
 it("Dynamic Forms Component Exists", () => {
   const wrapped = shallow(<App />);
   expect(wrapped.find(<SubmitDynamicForms />).length);
+});
+
+it("has a form", () => {
+  const wrapped = shallow(<App />);
+  const wrappedDyn = wrapped.shallow(<SubmitDynamicForms />);
+  expect(wrappedDyn.find("form").length);
+});
+
+it("has an input", () => {
+  const wrapped = shallow(<App />);
+  const wrappedDyn = wrapped.shallow(<SubmitDynamicForms />);
+  expect(wrappedDyn.find("input").length);
+});
+
+it("has a button", () => {
+  const wrapped = shallow(<App />);
+  const wrappedDyn = wrapped.shallow(<SubmitDynamicForms />);
+  expect(wrappedDyn.find("button").length);
 });
